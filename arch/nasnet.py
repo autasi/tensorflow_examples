8fcea938d9f13b857c2dbfefd1dd79b9be7f5ce3
Akos Utasi <akos.utasi@realeyesit.com>
2017-12-30 09:33:06 +0100

    nasnet file

---
+++ arch/nasnet.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 29 22:42:06 2017
+
+@author: autasi
+"""
+
+#Learning Transferable Architectures for Scalable Image Recognition
+#https://arxiv.org/pdf/1707.07012.pdf
