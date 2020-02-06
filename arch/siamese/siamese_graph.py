0bfbb38210950061128eae91d24368d51f05b018
Akos Utasi <akos.utasi@aimotive.com>
2020-02-06 09:33:05 +0100

    siamese

--- arch/siamese/siamese_graph.py
+++ arch/siamese/siamese_graph.py
@@ -3,7 +3,7 @@
 
 
 import tensorflow as tf
-from arch.layers import conv2d, conv2d_relu, conv2d_bn_relu, max_pool2d, dense_sigmoid, flatten, spatial_softmax
+from arch.layers import conv2d, conv2d_relu, max_pool2d, flatten
 from arch.initializers import He_normal
 
 
@@ -84,91 +84,3 @@ def mnist_siamese(x1, x2, weight_decay = 0.0001, seed = 42):
         layers.append(('output', (layers1, layers2)))
     
     return layers, variables    
-
-
-
-def landmark_net(x, K = 8, training = False, weight_decay = 0.0001, seed = 42):
-    layers = []
-    variables = []
-    
-    conv1 = conv2d_bn_relu(
-            x, size = 5, n_filters = 20,
-            training = training,
-            regularizer = tf.contrib.layers.l2_regularizer(weight_decay),
-            kernel_init = He_normal(seed = seed+1),
-            name = "conv1")
-    layers.append(("conv1", conv1))
-
-    pool1 = max_pool2d(conv1, size = 2, stride = 2, name = "pool1")
-    layers.append(("pool1", pool1))
-
-    conv2 = conv2d_bn_relu(
-            pool1, size = 3, n_filters = 48,
-            training = training,
-            regularizer = tf.contrib.layers.l2_regularizer(weight_decay),
-            kernel_init = He_normal(seed = seed+2),
-            name = "conv2")
-    layers.append(("conv2", conv2))
-
-    conv3 = conv2d_bn_relu(
-            conv2, size = 3, n_filters = 64,
-            training = training,
-            regularizer = tf.contrib.layers.l2_regularizer(weight_decay),
-            kernel_init = He_normal(seed = seed+3),
-            name = "conv3")
-    layers.append(("conv3", conv3))
-
-    conv4 = conv2d_bn_relu(
-            conv3, size = 3, n_filters = 80,
-            training = training,
-            regularizer = tf.contrib.layers.l2_regularizer(weight_decay),
-            kernel_init = He_normal(seed = seed+4),
-            name = "conv4")
-    layers.append(("conv4", conv4))
-
-    conv5 = conv2d_bn_relu(
-            conv4, size = 3, n_filters = 256,
-            training = training,
-            regularizer = tf.contrib.layers.l2_regularizer(weight_decay),
-            kernel_init = He_normal(seed = seed+5),
-            name = "conv5")
-    layers.append(("conv5", conv5))
-
-    conv6 = conv2d_bn_relu(
-            conv5, size = 3, n_filters = K,
-            training = training,
-            regularizer = tf.contrib.layers.l2_regularizer(weight_decay),
-            kernel_init = He_normal(seed = seed+6),
-            name = "conv6")
-    layers.append(("conv6", conv6))
-    
-    sp_softmax = spatial_softmax(conv6, name = "spatial_softmax")
-    #sp_softmax = tf.contrib.layers.spatial_softmax(conv6)
-    layers.append(("output", sp_softmax))
-
-
-    return layers, variables
-
-
-def landmark_siamese(x1, x2, K = 8, weight_decay = 0.0001, seed = 42):
-    layers = []
-    variables = []
-
-    training = tf.placeholder(tf.bool, name="training")
-    variables.append(("training", training))
-    
-    
-    with tf.variable_scope("siamese") as scope:    
-        # network 1
-        layers1, variables1 = landmark_net(x1, K=K, training=training, weight_decay=weight_decay, seed=seed+0)
-    
-        scope.reuse_variables()
-        
-        # network 2
-        layers2, variables2 = mnist_siamese_base(x2, K=K, training=training, weight_decay=weight_decay, seed=seed+len(layers1))
-        
-        layers.append(('output', (layers1, layers2)))
-    
-    return layers, variables    
-
-
